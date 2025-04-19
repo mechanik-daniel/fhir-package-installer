576a16ff547a631eb42dfeee720b81bdf205b3f3
mechanik-daniel <daniel@outburn.co.il>
2025-04-20 01:30:25 +0300

    test: migrate to class based

--- test/fhir-package-installer.test.ts
+++ test/fhir-package-installer.test.ts
@@ -1,94 +1,95 @@
 import path from 'path';
 import fs from 'fs-extra';
-import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 
-import {
-  getCachePath,
-  getPackageDirPath,
-  getPackageIndexFile,
-  isInstalled,
-  setLogger,
-  install,
-  checkLatestPackageDist,
-  getManifest,
-  toPackageObject,
-  getDependencies
-} from 'fhir-package-installer';
-
+import fpi, { FhirPackageInstaller } from 'fhir-package-installer';
 import type { ILogger } from 'fhir-package-installer';
+
+const noopLogger: ILogger = {
+  info: () => {},
+  warn: () => {},
+  error: () => {},
+};
 
 describe('fhir-package-installer module', () => {
   const fakePackage = { id: 'fake-package', version: '1.0.0' };
-  const fumePkg = { id: 'fume.outburn.r4', version: '0.1.1' };
-  const fumePkgDir = getPackageDirPath(fumePkg);
+  const testPkg = { id: 'hl7.fhir.uv.sdc', version: '3.0.0' };
+
+  const silentFpi = new FhirPackageInstaller({ logger: noopLogger });
+  const customCachePath = path.join(__dirname, '.test-cache');
+  const customCacheFpi = new FhirPackageInstaller({
+    cachePath: customCachePath
+  });
 
   beforeAll(async () => {
     // cleanup before running tests
-    await fs.remove(fumePkgDir);
+    await fs.remove(customCachePath);
   });
 
   it('should return correct package directory path', () => {
-    const expectedPath = path.join(getCachePath(), 'fake-package#1.0.0');
-    expect(getPackageDirPath(fakePackage)).toBe(expectedPath);
+    const expectedPath = path.join(fpi.getCachePath(), 'fake-package#1.0.0');
+    expect(fpi.getPackageDirPath(fakePackage)).toBe(expectedPath);
+  });
+
+  it('should return correct package directory path (custom)', () => {
+    const expectedPath = path.join(customCachePath, 'fake-package#1.0.0');
+    expect(customCacheFpi.getPackageDirPath(fakePackage)).toBe(expectedPath);
   });
 
   it('should throw ENOENT on getPackageIndexFile for fake package', async () => {
-    const noopLogger: ILogger = {
-      info: () => {},
-      warn: () => {},
-      error: () => {},
-    };
-    setLogger(noopLogger);
-
-    await expect(getPackageIndexFile(fakePackage)).rejects.toMatchObject({ code: 'ENOENT' });
-
-    setLogger(); // reset logger
+    await expect(silentFpi.getPackageIndexFile(fakePackage)).rejects.toMatchObject({ code: 'ENOENT' });
   });
 
   it('should return false for isInstalled on fake package', () => {
-    expect(isInstalled(fakePackage)).toBe(false);
+    expect(fpi.isInstalled(fakePackage)).toBe(false);
   });
 
   it('should return false for isInstalled on deleted real package', () => {
-    expect(isInstalled(fumePkg)).toBe(false);
+    expect(customCacheFpi.isInstalled(testPkg)).toBe(false);
   });
 
-  it('should correctly detect latest available version of fume package', async () => {
-    const latest = await checkLatestPackageDist(fumePkg.id);
-    expect(latest).toBe(fumePkg.version);
+  it('should correctly detect latest available version of test package', async () => {
+    const latest = await fpi.checkLatestPackageDist(testPkg.id);
+    expect(latest).toBe(testPkg.version);
   });
 
-  it('should install the fume package successfully', async () => {
-    const result = await install(fumePkg);
+  it('should install the test package successfully', async () => {
+    const result = await customCacheFpi.install(testPkg);
     expect(result).toBe(true);
-  }, 90000); // 90 seconds timeout for installation
+  }, 240000); // 240 seconds timeout for installation
 
   it('should get a valid package index file after install', async () => {
-    const index = await getPackageIndexFile(fumePkg);
+    const index = await customCacheFpi.getPackageIndexFile(testPkg);
     expect(index).toMatchObject({
       'index-version': 2,
     });
   });
 
   it('should get a valid manifest file after install', async () => {
-    const manifest = await getManifest(fumePkg);
-    expect(manifest.name).toBe(fumePkg.id);
+    const manifest = await customCacheFpi.getManifest(testPkg);
+    expect(manifest.name).toBe(testPkg.id);
   });
 
   it('should parse a package string to a valid package object', async () => {
-    const obj = await toPackageObject('pkg.name@1.0.0');
+    const obj = await fpi.toPackageObject('pkg.name@1.0.0');
     expect(obj).toEqual({ id: 'pkg.name', version: '1.0.0' });
   });
 
-  it('should get valid dependencies from the fume package', async () => {
-    const deps = await getDependencies(fumePkg);
+  it('should get valid dependencies from the test package', async () => {
+    const deps = await customCacheFpi.getDependencies(testPkg);
     expect(deps).toMatchObject({
-      'il.core.fhir.r4': '0.16.1'
+      'hl7.fhir.r4.core': '4.0.1',
+      'hl7.fhir.r4.examples': '4.0.1'
     });
   });
 
-  afterAll(async () => {
-    // optional cleanup
-    await fs.remove(fumePkgDir);
+  it('should get true for isInstalled on all dependencies of test package', async () => {
+    expect({
+      'hl7.fhir.r4.core': customCacheFpi.isInstalled({ id: 'hl7.fhir.r4.core', version: '4.0.1' }),
+      'hl7.fhir.r4.examples': customCacheFpi.isInstalled({ id: 'hl7.fhir.r4.examples', version: '4.0.1' })
+    }).toMatchObject({
+      'hl7.fhir.r4.core': true,
+      'hl7.fhir.r4.examples': true
+    });
   });
 });
